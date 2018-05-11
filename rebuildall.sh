5831369a4fc2fc073ace245e9293b7af0e6aacd2
McKayne <mc.kayne@yahoo.com>
2018-05-12 00:38:29 +0300

    12 May 2

--- rebuildall.sh
+++
@@ -1,4 +0,0 @@
-mkoctfile osc_deadening.cc -lOpenCL
-mkoctfile osc_deadening.cc -o energyInt -lOpenCL
-mkoctfile osc_deadening.cc -o gpuGradientAt -lOpenCL
-mkoctfile osc_deadening.cc -o gpuHessianAt -lOpenCL
--- rebuildall_noopencl.sh
+++
@@ -1,4 +0,0 @@
-mkoctfile oscDeadening.cc
-mkoctfile oscDeadening.cc -o energyInt
-mkoctfile oscDeadening.cc -o gpuGradientAt
-mkoctfile oscDeadening.cc -o gpuHessianAt
